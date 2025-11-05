d24e376a4820f9e87ed12e8e7348c07f9a717291
Leon Oostrum <l.oostrum@esciencecenter.nl>
2025-11-05 14:47:06 +0100

    Implement use of multiple blocks for M/N

--- kernels/kernel_complex_interleaved.cu
+++ kernels/kernel_complex_interleaved.cu
@@ -14,140 +14,163 @@ using A_eff_t = Tin[M_GLOBAL][K_GLOBAL * COMPLEX];
 using B_eff_t = Tin[N_GLOBAL * 2][K_GLOBAL * COMPLEX];
 using C_eff_t = Tout[M_GLOBAL][N_GLOBAL * COMPLEX];
 
-extern "C" __global__ void wmma_complex_gemm_basic_interleaved(C_t C, const A_t A, const B_t B) {
-    const unsigned tid = blockDim.y * blockDim.x * threadIdx.z + blockDim.x * threadIdx.y + threadIdx.x;
-    const unsigned block_size = blockDim.x * blockDim.y * blockDim.z;
+extern "C" __global__ void
+wmma_complex_gemm_basic_interleaved(C_t C, const A_t A, const B_t B) {
+  const unsigned blockN = blockIdx.x;
+  const unsigned blockM = blockIdx.y;
+  const unsigned warpN = threadIdx.y;
+  const unsigned warpM = threadIdx.z;
 
-    __shared__ Tin A_s[M_GLOBAL][K_GLOBAL][COMPLEX];
-    __shared__ Tin B_s[N_GLOBAL][2][K_GLOBAL][COMPLEX];
+  const unsigned tid = blockDim.x * blockDim.y * threadIdx.z +
+                       blockDim.x * threadIdx.y + threadIdx.x;
+  const unsigned block_size = blockDim.x * blockDim.y * blockDim.z;
 
-    for (unsigned i = tid; i < M_GLOBAL * K_GLOBAL * COMPLEX; i+= block_size) {
-        const unsigned c = i % COMPLEX;
-        const unsigned k = (i / COMPLEX) % K_GLOBAL;
-        const unsigned m = (i / (COMPLEX * K_GLOBAL));
+  const unsigned block_m_start = blockM * M_PER_BLOCK;
+  const unsigned block_n_start = blockN * N_PER_BLOCK;
 
-        A_s[m][k][c] = A[m][k][c];
+  __shared__ Tin A_s[M_PER_BLOCK][K_PER_BUFFER][COMPLEX];
+  __shared__ Tin B_s[N_PER_BLOCK][2][K_PER_BUFFER][COMPLEX];
+
+  for (unsigned i = tid; i < M_PER_BLOCK * K_PER_BUFFER * COMPLEX;
+       i += block_size) {
+    const unsigned c = i % COMPLEX;
+    const unsigned k = (i / COMPLEX) % K_PER_BUFFER;
+    const unsigned m = (i / (COMPLEX * K_PER_BUFFER));
+
+    A_s[m][k][c] = A[block_m_start + m][k][c];
+  }
+
+  for (unsigned i = tid; i < N_PER_BLOCK * K_PER_BUFFER; i += block_size) {
+    const unsigned k = i % K_PER_BUFFER;
+    const unsigned n = i / K_PER_BUFFER;
+
+    B_s[n][0][k][REAL] = B[block_n_start + n][k][REAL];
+    B_s[n][0][k][IMAG] = -B[block_n_start + n][k][IMAG];
+
+    B_s[n][1][k][REAL] = B[block_n_start + n][k][IMAG];
+    B_s[n][1][k][IMAG] = B[block_n_start + n][k][REAL];
+  }
+  __syncthreads();
+
+  // pretend we do a matmul with N_ = N * 2 and K_ = K * COMPLEX;
+  const unsigned M_ = M_PER_BLOCK;
+  const unsigned N_ = N_PER_BLOCK * 2;
+  const unsigned K_ = K_PER_BUFFER * COMPLEX;
+
+  const A_eff_t *A_ = reinterpret_cast<const A_eff_t *>(A_s);
+  const B_eff_t *B_ = reinterpret_cast<const B_eff_t *>(B_s);
+  C_eff_t *C_ = reinterpret_cast<C_eff_t *>(C);
+
+  for (unsigned n = 0; n < N_; n += N_WMMA) {
+    for (unsigned m = 0; m < M_; m += M_WMMA) {
+      rocwmma::fragment<rocwmma::accumulator, M_WMMA, N_WMMA, K_WMMA, Tout>
+          fragC;
+      rocwmma::fill_fragment(fragC, static_cast<Tout>(0));
+      for (unsigned k = 0; k < K_; k += K_WMMA) {
+        rocwmma::fragment<rocwmma::matrix_a, M_WMMA, N_WMMA, K_WMMA, Tin,
+                          rocwmma::row_major>
+            fragA;
+        rocwmma::fragment<rocwmma::matrix_b, M_WMMA, N_WMMA, K_WMMA, Tin,
+                          rocwmma::col_major>
+            fragB;
+        rocwmma::load_matrix_sync(fragA, &(*A_)[m][k], K_);
+        rocwmma::load_matrix_sync(fragB, &(*B_)[n][k], K_);
+        rocwmma::mma_sync(fragC, fragA, fragB, fragC);
+      }
+      rocwmma::store_matrix_sync(
+          &(*C_)[block_m_start + m][block_n_start * COMPLEX + n], fragC,
+          N_GLOBAL * COMPLEX, rocwmma::mem_row_major);
     }
+  }
+  __syncthreads();
 
-    for (unsigned i = tid; i < N_GLOBAL * K_GLOBAL; i += block_size) {
-        const unsigned k = i % K_GLOBAL;
-        const unsigned n = i / K_GLOBAL;
+  // rocwmma::load_matrix_sync(fragA, &A[0][0][0], K_GLOBAL * COMPLEX);
+  // rocwmma::load_matrix_sync(fragB, &B[0][0][0], K_GLOBAL * COMPLEX);
 
-        B_s[n][0][k][REAL] = B[n][k][REAL];
-        B_s[n][0][k][IMAG] = -B[n][k][IMAG];
+  /*
+  const size_t blockN = blockIdx.x;
+  const size_t blockM = blockIdx.y;
+  const size_t warpN = threadIdx.y;
+  const size_t warpM = threadIdx.z;
 
-        B_s[n][1][k][REAL] = B[n][k][IMAG];
-        B_s[n][1][k][IMAG] = B[n][k][REAL];
-    }
-    __syncthreads();
+  const size_t block_size = blockDim.x * blockDim.y * blockDim.z;
+  const size_t tid = threadIdx.x + blockDim.x * threadIdx.y + blockDim.x *
+  blockDim.y * threadIdx.z;
 
-    // pretend we do a matmul with N_ = N * 2 and K_ = K * COMPLEX;
-    const unsigned M_ = M_GLOBAL;  // unchanged
-    const unsigned N_ = N_GLOBAL * 2;
-    const unsigned K_ = K_GLOBAL * COMPLEX;
+  __shared__ Tin A_s[BLOCK_TILE_SIZE_M][TILE_SIZE_K][COMPLEX];
+  __shared__ Tin B_s[BLOCK_TILE_SIZE_N][TILE_SIZE_K][COMPLEX];
 
-    const A_eff_t *A_ = reinterpret_cast<const A_eff_t *>(A_s);
-    const B_eff_t *B_ = reinterpret_cast<const B_eff_t *>(B_s);
-    const C_eff_t *C_ = reinterpret_cast<const C_eff_t *>(C);
+  const unsigned block_start_m = blockM * BLOCK_TILE_SIZE_M;
+  const unsigned block_start_n = blockN * BLOCK_TILE_SIZE_N;
 
-    for (unsigned n = 0; n < N_; n += N_WMMA) {
-        for (unsigned m = 0; m < M_; m += M_WMMA) {
-            rocwmma::fragment<rocwmma::accumulator, M_WMMA, N_WMMA, K_WMMA, Tout> fragC;
-            rocwmma::fill_fragment(fragC, static_cast<Tout>(0));
-            for (unsigned k = 0; k < K_; k += K_WMMA) {
-                rocwmma::fragment<rocwmma::matrix_a, M_WMMA, N_WMMA, K_WMMA, Tin, rocwmma::row_major> fragA;
-                rocwmma::fragment<rocwmma::matrix_b, M_WMMA, N_WMMA, K_WMMA, Tin, rocwmma::col_major> fragB;
-                rocwmma::load_matrix_sync(fragA,  &(*A_)[m][k], K_);
-                rocwmma::load_matrix_sync(fragB,  &(*B_)[n][k], K_);
-                rocwmma::mma_sync(fragC, fragA, fragB, fragC);
-            }
-            rocwmma::store_matrix_sync(&(*C)[m][n], fragC, N_, rocwmma::mem_row_major);
-        }
-    }
-    __syncthreads();
+  const unsigned warp_tiles_m = WARP_TILE_SIZE_M / M_WMMA;
+  const unsigned warp_tiles_n = WARP_TILE_SIZE_N / N_WMMA;
 
+  rocwmma::fragment<rocwmma::accumulator, M_WMMA, N_WMMA, K_WMMA, Tout>
+  fragC[warp_tiles_m][warp_tiles_n]; for (unsigned n = 0; n < warp_tiles_n; n++)
+  { for (unsigned m = 0; m < warp_tiles_m; m++) {
+          rocwmma::fill_fragment(fragC[m][n], static_cast<Tin>(0.0f));
+      }
+  }
 
-    // rocwmma::load_matrix_sync(fragA, &A[0][0][0], K_GLOBAL * COMPLEX);
-    // rocwmma::load_matrix_sync(fragB, &B[0][0][0], K_GLOBAL * COMPLEX);
+  // todo: padding in K
+  for (unsigned k_tile = 0; k_tile < K_GLOBAL / TILE_SIZE_K; k_tile++) {
+      const unsigned k_start = k_tile * TILE_SIZE_K;
 
-    /*
-    const size_t blockN = blockIdx.x;
-    const size_t blockM = blockIdx.y;
-    const size_t warpN = threadIdx.y;
-    const size_t warpM = threadIdx.z;
+      // load A and B into shared memory
+      for (unsigned i = 0; i < BLOCK_TILE_SIZE_M * TILE_SIZE_K * COMPLEX; i +=
+  block_size) { const unsigned c = i % COMPLEX; const unsigned k = (i / c) %
+  TILE_SIZE_K; const unsigned m = (i / (k + c)); A_s[m][k][c] = A[block_start_m
+  + m][k_start + k][c];
+      }
 
-    const size_t block_size = blockDim.x * blockDim.y * blockDim.z;
-    const size_t tid = threadIdx.x + blockDim.x * threadIdx.y + blockDim.x * blockDim.y * threadIdx.z;
+      for (unsigned i = 0; i < BLOCK_TILE_SIZE_N * TILE_SIZE_K * COMPLEX; i +=
+  block_size) { const unsigned c = i % COMPLEX; const unsigned k = (i / c) %
+  TILE_SIZE_K; const unsigned n = (i / (k + c)); B_s[n][k][c] = A[block_start_n
+  + n][k_start + k][c];
+      }
 
-    __shared__ Tin A_s[BLOCK_TILE_SIZE_M][TILE_SIZE_K][COMPLEX];
-    __shared__ Tin B_s[BLOCK_TILE_SIZE_N][TILE_SIZE_K][COMPLEX];
+      __syncthreads();
 
-    const unsigned block_start_m = blockM * BLOCK_TILE_SIZE_M;
-    const unsigned block_start_n = blockN * BLOCK_TILE_SIZE_N;
+      // run mma per K tile, note that we load 2 values per K due to complex
+  axis for (unsigned k = 0; k < TILE_SIZE_K; k+= K_WMMA / COMPLEX) {
+          // load MMA fragments
+          rocwmma::fragment<rocwmma::matrix_a, M_WMMA, N_WMMA, K_WMMA, Tin,
+  rocwmma::row_major> fragA[warp_tiles_m]; rocwmma::fragment<rocwmma::matrix_b,
+  M_WMMA, N_WMMA, K_WMMA, Tin, rocwmma::col_major> fragB[warp_tiles_n];
 
-    const unsigned warp_tiles_m = WARP_TILE_SIZE_M / M_WMMA;
-    const unsigned warp_tiles_n = WARP_TILE_SIZE_N / N_WMMA;
+          for (unsigned warp_tile_m = 0; warp_tile_m < warp_tiles_m; warp_tile_m
+  ++) { rocwmma::load_matrix_sync(fragA[warp_tile_m], &A_s[warp_tile_m *
+  WARP_TILE_SIZE_M][k][0], COMPLEX * TILE_SIZE_K);
+          }
 
-    rocwmma::fragment<rocwmma::accumulator, M_WMMA, N_WMMA, K_WMMA, Tout> fragC[warp_tiles_m][warp_tiles_n];
-    for (unsigned n = 0; n < warp_tiles_n; n++) {
-        for (unsigned m = 0; m < warp_tiles_m; m++) {
-            rocwmma::fill_fragment(fragC[m][n], static_cast<Tin>(0.0f));
-        }
-    }
+          for (unsigned warp_tile_n = 0; warp_tile_n < warp_tiles_n; warp_tile_n
+  ++) { rocwmma::load_matrix_sync(fragB[warp_tile_n], &A_s[warp_tile_n *
+  WARP_TILE_SIZE_N][k][0], COMPLEX * TILE_SIZE_K);
+          }
 
-    // todo: padding in K
-    for (unsigned k_tile = 0; k_tile < K_GLOBAL / TILE_SIZE_K; k_tile++) {
-        const unsigned k_start = k_tile * TILE_SIZE_K;
+          // run MMA
+          for (unsigned n = 0; n < warp_tiles_n; n++) {
+              for (unsigned m = 0; m < warp_tiles_m; m++) {
+                  rocwmma::mma_sync(fragC[m][n], fragA[m], fragB[n],
+  fragC[m][n]);
+              }
+          }
+      }
 
-        // load A and B into shared memory
-        for (unsigned i = 0; i < BLOCK_TILE_SIZE_M * TILE_SIZE_K * COMPLEX; i += block_size) {
-            const unsigned c = i % COMPLEX;
-            const unsigned k = (i / c) % TILE_SIZE_K;
-            const unsigned m = (i / (k + c));
-            A_s[m][k][c] = A[block_start_m + m][k_start + k][c];
-        }
+      // K loop done, store result
+      for (unsigned n = 0; n < warp_tiles_n; n++) {
+              for (unsigned m = 0; m < warp_tiles_m; m++) {
+                  rocwmma::store_matrix_sync(&C[block_start_m + m *
+  WARP_TILE_SIZE_M][block_start_n + n * WARP_TILE_SIZE_N][0], fragC[m][n],
+  N_GLOBAL * COMPLEX, rocwmma::mem_row_major);
+          }
+      }
 
-        for (unsigned i = 0; i < BLOCK_TILE_SIZE_N * TILE_SIZE_K * COMPLEX; i += block_size) {
-            const unsigned c = i % COMPLEX;
-            const unsigned k = (i / c) % TILE_SIZE_K;
-            const unsigned n = (i / (k + c));
-            B_s[n][k][c] = A[block_start_n + n][k_start + k][c];
-        }
+      // ensure all threads are done before new data is written to shared memory
+  in next loop iteration
+      __syncthreads();
+  }
 
-        __syncthreads();
-
-        // run mma per K tile, note that we load 2 values per K due to complex axis
-        for (unsigned k = 0; k < TILE_SIZE_K; k+= K_WMMA / COMPLEX) {
-            // load MMA fragments
-            rocwmma::fragment<rocwmma::matrix_a, M_WMMA, N_WMMA, K_WMMA, Tin, rocwmma::row_major> fragA[warp_tiles_m];
-            rocwmma::fragment<rocwmma::matrix_b, M_WMMA, N_WMMA, K_WMMA, Tin, rocwmma::col_major> fragB[warp_tiles_n];
-
-            for (unsigned warp_tile_m = 0; warp_tile_m < warp_tiles_m; warp_tile_m ++) {
-                rocwmma::load_matrix_sync(fragA[warp_tile_m], &A_s[warp_tile_m * WARP_TILE_SIZE_M][k][0], COMPLEX * TILE_SIZE_K);
-            }
-
-            for (unsigned warp_tile_n = 0; warp_tile_n < warp_tiles_n; warp_tile_n ++) {
-                rocwmma::load_matrix_sync(fragB[warp_tile_n], &A_s[warp_tile_n * WARP_TILE_SIZE_N][k][0], COMPLEX * TILE_SIZE_K);
-            }
-
-            // run MMA
-            for (unsigned n = 0; n < warp_tiles_n; n++) {
-                for (unsigned m = 0; m < warp_tiles_m; m++) {
-                    rocwmma::mma_sync(fragC[m][n], fragA[m], fragB[n], fragC[m][n]);
-                }
-            }
-        }
-
-        // K loop done, store result
-        for (unsigned n = 0; n < warp_tiles_n; n++) {
-                for (unsigned m = 0; m < warp_tiles_m; m++) {
-                    rocwmma::store_matrix_sync(&C[block_start_m + m * WARP_TILE_SIZE_M][block_start_n + n * WARP_TILE_SIZE_N][0], fragC[m][n], N_GLOBAL * COMPLEX, rocwmma::mem_row_major);
-            }
-        }
-
-        // ensure all threads are done before new data is written to shared memory in next loop iteration
-        __syncthreads();
-    }
-
-    */
+  */
 }
--- src/complex_interleaved.cpp
+++ src/complex_interleaved.cpp
@@ -10,12 +10,12 @@
 
 #include "kernel_complex_interleaved.cu.o.h"
 
-template<typename T>
-inline T align(T a, T b) {
+template <typename T> inline T align(T a, T b) {
   return ccglib::helper::ceildiv(a, b) * b;
 }
 
-float run(cu::Stream &stream, ccglib::pipeline::Pipeline &pipeline, cu::DeviceMemory &d_a, cu::DeviceMemory &d_b, cu::DeviceMemory &d_c) {
+float run(cu::Stream &stream, ccglib::pipeline::Pipeline &pipeline,
+          cu::DeviceMemory &d_a, cu::DeviceMemory &d_b, cu::DeviceMemory &d_c) {
   // run the kernels
   cu::Event start, end;
   stream.record(start);
@@ -36,25 +36,25 @@ int main(int argc, char *argv[]) {
   // const unsigned global_m = 8192;
   // const unsigned global_n = 8192;
   // const unsigned global_k = 8192;
-  const unsigned global_m = 16;
-  const unsigned global_n = 16;
+  const unsigned global_m = 128;
+  const unsigned global_n = 128;
   const unsigned global_k = 16;
   const unsigned batch_size = 1;
   const unsigned COMPLEX = 2;
   const unsigned REAL = 0;
   const unsigned IMAG = 1;
 
-  const double ops = 8ULL * 1e-9 * global_m * global_n * global_k;
+  const double ops = 8ULL * 1e-9 * global_m * global_n * global_k * batch_size;
 
   using Tin = half;
   using Tout = float;
 
   const size_t bytes_a =
-      sizeof(Tin) * batch_size * COMPLEX * global_m * global_k;
+      sizeof(Tin) * batch_size * global_m * global_k * COMPLEX;
   const size_t bytes_b =
-      sizeof(Tin) * batch_size * COMPLEX * global_n * global_k;
+      sizeof(Tin) * batch_size * global_n * global_k * COMPLEX;
   const size_t bytes_c =
-      sizeof(Tout) * batch_size * COMPLEX * global_m * global_n;
+      sizeof(Tout) * batch_size * global_m * global_n * COMPLEX;
 
   // Build the experimental kernel
   const std::string kernel_name = "wmma_complex_gemm_basic_interleaved";
@@ -65,8 +65,8 @@ int main(int argc, char *argv[]) {
   const unsigned warp_size = device.getAttribute(CU_DEVICE_ATTRIBUTE_WARP_SIZE);
 
   // tuning parameters
-  const unsigned M_PER_BLOCK = 16;
-  const unsigned N_PER_BLOCK = 16;
+  const unsigned M_PER_BLOCK = 64;
+  const unsigned N_PER_BLOCK = 64;
   const unsigned M_PER_WARP = 16;
   const unsigned N_PER_WARP = 16;
   const unsigned K_PER_BUFFER = 16;
@@ -76,11 +76,15 @@ int main(int argc, char *argv[]) {
   const unsigned N_WMMA = 16;
   const unsigned K_WMMA = 16;
 
-  dim3 grid{ccglib::helper::ceildiv(global_n, N_PER_BLOCK), ccglib::helper::ceildiv(global_m, M_PER_BLOCK), 1};
-  dim3 threads{warp_size, ccglib::helper::ceildiv(N_PER_WARP, N_WMMA), ccglib::helper::ceildiv(M_PER_WARP, M_WMMA)};
+  dim3 grid{ccglib::helper::ceildiv(global_n, N_PER_BLOCK),
+            ccglib::helper::ceildiv(global_m, M_PER_BLOCK), batch_size};
+  dim3 threads{warp_size, ccglib::helper::ceildiv(N_PER_WARP, N_WMMA),
+               ccglib::helper::ceildiv(M_PER_WARP, M_WMMA)};
 
-  std::cout << "block size: " << threads.x << " " << threads.y << " " << threads.z << std::endl;
-  std::cout << "grid size: " << grid.x << " " << grid.y << " " << grid.z << std::endl;
+  std::cout << "block size: " << threads.x << " " << threads.y << " "
+            << threads.z << std::endl;
+  std::cout << "grid size: " << grid.x << " " << grid.y << " " << grid.z
+            << std::endl;
 
   std::vector<std::string> options = {
     "-std=c++17",
@@ -105,7 +109,8 @@ int main(int argc, char *argv[]) {
     "-DK_WMMA=" + std::to_string(K_WMMA)
   };
 
-  nvrtc::Program program(kernel_complex_interleaved_source, "kernel_complex_interleaved.cu");
+  nvrtc::Program program(kernel_complex_interleaved_source,
+                         "kernel_complex_interleaved.cu");
   try {
     program.compile(options);
   } catch (nvrtc::Error &error) {
@@ -128,10 +133,10 @@ int main(int argc, char *argv[]) {
   // Create input data
   auto generator = std::bind(std::uniform_real_distribution<float>(-10, 10),
                              std::default_random_engine());
-  for (unsigned i = 0; i < global_m * global_k * COMPLEX; i ++) {
+  for (unsigned i = 0; i < batch_size * global_m * global_k * COMPLEX; i++) {
     static_cast<Tin *>(h_a)[i] = static_cast<Tin>(generator());
   }
-  for (unsigned i = 0; i < global_n * global_k * COMPLEX; i ++) {
+  for (unsigned i = 0; i < batch_size * global_n * global_k * COMPLEX; i++) {
     static_cast<Tin *>(h_b)[i] = static_cast<Tin>(generator());
   }
 
@@ -151,7 +156,8 @@ int main(int argc, char *argv[]) {
 
   cu::Event start, end;
   stream.record(start);
-  stream.launchKernel(function, grid.x, grid.y, grid.z, threads.x, threads.y, threads.z, 0, parameters);
+  stream.launchKernel(function, grid.x, grid.y, grid.z, threads.x, threads.y,
+                      threads.z, 0, parameters);
   stream.record(end);
   stream.synchronize();
 
@@ -167,17 +173,16 @@ int main(int argc, char *argv[]) {
   std::cout << "TFLOPS: " << tflops << std::endl;
 
   // compare to existing ccglib pipelines
-  ccglib::pipeline::Pipeline pipeline_opt_planar(batch_size, global_m, global_n, global_k, device, stream,
-                                                 // input/output complex axis location
-                                                 ccglib::complex_planar, ccglib::complex_planar,
-                                                 // a, b, c mem order
-                                                 ccglib::mma::row_major, ccglib::mma::col_major, ccglib::mma::row_major,
-                                                 // input/output precision
-                                                 ccglib::float16, ccglib::float32,
-                                                 // kernel variant
-                                                 ccglib::mma::opt
-                                                 );
-
+  ccglib::pipeline::Pipeline pipeline_opt_planar(
+      batch_size, global_m, global_n, global_k, device, stream,
+      // input/output complex axis location
+      ccglib::complex_planar, ccglib::complex_planar,
+      // a, b, c mem order
+      ccglib::mma::row_major, ccglib::mma::col_major, ccglib::mma::row_major,
+      // input/output precision
+      ccglib::float16, ccglib::float32,
+      // kernel variant
+      ccglib::mma::opt);
 
   d_c.zero(bytes_c);
   runtime = run(stream, pipeline_opt_planar, d_a, d_b, d_c);
@@ -188,16 +193,16 @@ int main(int argc, char *argv[]) {
   std::cout << "runtime: " << runtime << " ms" << std::endl;
   std::cout << "TFLOPS: " << tflops << std::endl;
 
-  ccglib::pipeline::Pipeline pipeline_opt_interleaved(batch_size, global_m, global_n, global_k, device, stream,
-                                                      // input/output complex axis location
-                                                      ccglib::complex_interleaved, ccglib::complex_interleaved,
-                                                      // a, b, c mem order
-                                                      ccglib::mma::row_major, ccglib::mma::col_major, ccglib::mma::row_major,
-                                                      // input/output precision
-                                                      ccglib::float16, ccglib::float32,
-                                                      // kernel variant
-                                                      ccglib::mma::opt
-                                                      );
+  ccglib::pipeline::Pipeline pipeline_opt_interleaved(
+      batch_size, global_m, global_n, global_k, device, stream,
+      // input/output complex axis location
+      ccglib::complex_interleaved, ccglib::complex_interleaved,
+      // a, b, c mem order
+      ccglib::mma::row_major, ccglib::mma::col_major, ccglib::mma::row_major,
+      // input/output precision
+      ccglib::float16, ccglib::float32,
+      // kernel variant
+      ccglib::mma::opt);
 
   d_c.zero(bytes_c);
   runtime = run(stream, pipeline_opt_interleaved, d_a, d_b, d_c);
@@ -213,15 +218,16 @@ int main(int argc, char *argv[]) {
 
   // Compare pipeline interleaved to new kernel
   unsigned errs = 0;
-  for (unsigned m = 0; m < global_m; m ++ ) {
+  for (unsigned m = 0; m < global_m; m++) {
     for (unsigned n = 0; n < global_n; n++) {
-      if (errs > 10) {
-          break;
-      }
       const unsigned idx = n * COMPLEX + m * global_n * COMPLEX;
-      const std::complex<Tout> value{static_cast<Tout *>(h_c)[idx + REAL], static_cast<Tout *>(h_c)[idx + IMAG]};
-      const std::complex<Tout> value_ref{static_cast<Tout *>(h_c_ref)[idx + REAL], static_cast<Tout *>(h_c_ref)[idx + IMAG]};
-      const Tout diff = std::max(std::abs(value.real() - value_ref.real()), std::abs(value.imag() - value_ref.imag()));
+      const std::complex<Tout> value{static_cast<Tout *>(h_c)[idx + REAL],
+                                     static_cast<Tout *>(h_c)[idx + IMAG]};
+      const std::complex<Tout> value_ref{
+          static_cast<Tout *>(h_c_ref)[idx + REAL],
+          static_cast<Tout *>(h_c_ref)[idx + IMAG]};
+      const Tout diff = std::max(std::abs(value.real() - value_ref.real()),
+                                 std::abs(value.imag() - value_ref.imag()));
       if (diff > .1) {
         errs++;
       }
@@ -235,17 +241,22 @@ int main(int argc, char *argv[]) {
     std::cout << "Result ok" << std::endl;
   }
 
-  if (true) {
+  if (errs > 0) {
     std::cout << std::endl;
-    for (unsigned m = 0; m < global_m; m ++ ) {
+    for (unsigned m = 0; m < global_m; m++) {
+      // print only first and last row
+      if (m != 0 && m != (global_m - 1))
+        continue;
       for (unsigned n = 0; n < global_n; n++) {
-          const unsigned idx = n * COMPLEX + m * global_n * COMPLEX;
-          std::cout << std::setw(12) << static_cast<Tout *>(h_c)[idx + REAL] << " + " << static_cast<Tout *>(h_c)[idx + IMAG] << "i";
-          std::cout << "   ";
-          std::cout << std::setw(12) << static_cast<Tout *>(h_c_ref)[idx + REAL] << " + " << static_cast<Tout *>(h_c_ref)[idx + IMAG] << "i";
-          std::cout << std::endl;
+        const unsigned idx = n * COMPLEX + m * global_n * COMPLEX;
+        std::cout << std::setw(12) << static_cast<Tout *>(h_c)[idx + REAL]
+                  << " + " << static_cast<Tout *>(h_c)[idx + IMAG] << "i";
+        std::cout << "   ";
+        std::cout << std::setw(12) << static_cast<Tout *>(h_c_ref)[idx + REAL]
+                  << " + " << static_cast<Tout *>(h_c_ref)[idx + IMAG] << "i";
+        std::cout << std::endl;
       }
-    break;
+      std::cout << std::endl;
     }
   }
 }
